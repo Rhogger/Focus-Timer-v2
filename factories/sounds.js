af03cdcd550b2c1ab9605f2774ca458d90ca9f21
rhogger <rhoggerrv@gmail.com>
2023-07-18 13:56:42 -0300

    :recycle: refactor: Sistema de som refatorado, Factory cumpre sua responsabilidade e o Script.js está mais limpo

--- factories/sounds.js
+++ factories/sounds.js
@@ -1,9 +1,66 @@
 export default function Sounds({
+  forestCard,
+  rainCard,
+  coffeeCard,
+  fireplaceCard,
   forestAudio,
   rainAudio,
   coffeeStoreAudio,
   fireplaceAudio,
+  removeSelectedStyle
 }) {
+
+  function playForestAudio() {
+    removeSelectedStyle(rainCard, coffeeCard, fireplaceCard)
+    pauseAudio(rainAudio, coffeeStoreAudio, fireplaceAudio)
+
+    if (!forestCard.classList.contains('selected')) {
+      forestCard.classList.add('selected')
+      forestAudio.play()
+    } else {
+      removeSelectedStyle(forestCard)
+      pauseAudio(forestAudio)
+    }
+  }
+
+  function playRainAudio() {
+    removeSelectedStyle(forestCard, coffeeCard, fireplaceCard)
+    pauseAudio(forestAudio, coffeeStoreAudio, fireplaceAudio)
+
+    if (!rainCard.classList.contains('selected')) {
+      rainCard.classList.add('selected')
+      rainAudio.play()
+    } else {
+      removeSelectedStyle(rainCard)
+      pauseAudio(rainAudio)
+    }
+  }
+
+  function playCoffeeStoreAudio() {
+    removeSelectedStyle(forestCard, rainCard, fireplaceCard)
+    pauseAudio(forestAudio, rainAudio, fireplaceAudio)
+
+    if (!coffeeCard.classList.contains('selected')) {
+      coffeeCard.classList.add('selected')
+      coffeeStoreAudio.play()
+    } else {
+      removeSelectedStyle(coffeeCard)
+      pauseAudio(coffeeStoreAudio)
+    }
+  }
+
+  function playFireplaceAudio() {
+    removeSelectedStyle(forestCard, rainCard, coffeeCard)
+    pauseAudio(forestAudio, coffeeStoreAudio, rainAudio)
+
+    if (!fireplaceCard.classList.contains('selected')) {
+      fireplaceCard.classList.add('selected')
+      fireplaceAudio.play()
+    } else {
+      removeSelectedStyle(fireplaceCard)
+      pauseAudio(fireplaceAudio)
+    }
+  }
 
   function pauseAudio(audio1, audio2, audio3) {
     audio1 = audio1 === undefined ? null : audio1.pause()
@@ -12,6 +69,10 @@ export default function Sounds({
   }
 
   return {
+    playForestAudio,
+    playRainAudio,
+    playCoffeeStoreAudio,
+    playFireplaceAudio,
     pauseAudio
   }
 }
--- script.js
+++ script.js
@@ -18,63 +18,24 @@ import Sounds from './factories/sounds.js'
 console.log('Script ativo');
 
 const sounds = Sounds({
+  forestCard,
+  rainCard,
+  coffeeCard,
+  fireplaceCard,
   forestAudio,
   rainAudio,
   coffeeStoreAudio,
   fireplaceAudio,
+  removeSelectedStyle
 })
 
-forestCard.addEventListener('click', () => {
-  removeSelectedStyle(rainCard, coffeeCard, fireplaceCard)
-  sounds.pauseAudio(rainAudio, coffeeStoreAudio, fireplaceAudio)
+forestCard.addEventListener('click', sounds.playForestAudio)
 
-  if (!forestCard.classList.contains('selected')) {
-    forestCard.classList.add('selected')
-    forestAudio.play()
-  } else {
-    removeSelectedStyle(forestCard)
-    sounds.pauseAudio(forestAudio)
-  }
-})
+rainCard.addEventListener('click', sounds.playRainAudio)
 
-rainCard.addEventListener('click', () => {
-  removeSelectedStyle(forestCard, coffeeCard, fireplaceCard)
-  sounds.pauseAudio(forestAudio, coffeeStoreAudio, fireplaceAudio)
+coffeeCard.addEventListener('click', sounds.playCoffeeStoreAudio)
 
-  if (!rainCard.classList.contains('selected')) {
-    rainCard.classList.add('selected')
-    rainAudio.play()
-  } else {
-    removeSelectedStyle(rainCard)
-    sounds.pauseAudio(rainAudio)
-  }
-})
-
-coffeeCard.addEventListener('click', () => {
-  removeSelectedStyle(forestCard, rainCard, fireplaceCard)
-  sounds.pauseAudio(forestAudio, rainAudio, fireplaceAudio)
-
-  if (!coffeeCard.classList.contains('selected')) {
-    coffeeCard.classList.add('selected')
-    coffeeStoreAudio.play()
-  } else {
-    removeSelectedStyle(coffeeCard)
-    sounds.pauseAudio(coffeeStoreAudio)
-  }
-})
-
-fireplaceCard.addEventListener('click', () => {
-  removeSelectedStyle(forestCard, rainCard, coffeeCard)
-  sounds.pauseAudio(forestAudio, coffeeStoreAudio, rainAudio)
-
-  if (!fireplaceCard.classList.contains('selected')) {
-    fireplaceCard.classList.add('selected')
-    fireplaceAudio.play()
-  } else {
-    removeSelectedStyle(fireplaceCard)
-    sounds.pauseAudio(fireplaceAudio)
-  }
-})
+fireplaceCard.addEventListener('click', sounds.playFireplaceAudio)
 
 function removeSelectedStyle(card1, card2, card3) {
   card1 = card1 === undefined ? null : card1.classList.remove('selected')
